8ce144ed2ffde914c74a739dc437477e3b07d52f
wrluu <fikbookmy@gmail.com>
2025-02-24 14:26:31 +0300

    поправила файл test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,118 +1,36 @@
+# test_generators.py
+
 import pytest
 from generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {
-            "amount": "43318.34",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160"
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {
-            "amount": "56883.54",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229"
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {
-            "amount": "67314.70",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
-        },
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657"
-    }
+    {"id": 1, "operationAmount": {"currency": {"code": "USD"}}, "description": "Описание 1"},
+    {"id": 2, "operationAmount": {"currency": {"code": "EUR"}}, "description": "Описание 2"},
+    {"id": 3, "operationAmount": {"currency": {"code": "USD"}}, "description": "Описание 3"},
 ]
 
 def test_filter_by_currency():
     usd_transactions = list(filter_by_currency(transactions, "USD"))
-    assert len(usd_transactions) == 3
+    assert len(usd_transactions) == 2
     assert all(t['operationAmount']['currency']['code'] == "USD" for t in usd_transactions)
 
-    rub_transactions = list(filter_by_currency(transactions, "RUB"))
-    assert len(rub_transactions) == 2
-    assert all(t['operationAmount']['currency']['code'] == "RUB" for t in rub_transactions)
+    eur_transactions = list(filter_by_currency(transactions, "EUR"))
+    assert len(eur_transactions) == 1
+    assert all(t['operationAmount']['currency']['code'] == "EUR" for t in eur_transactions)
 
-    empty_transactions = list(filter_by_currency(transactions, "EUR"))
+    empty_transactions = list(filter_by_currency(transactions, "RUB"))
     assert len(empty_transactions) == 0
 
 def test_transaction_descriptions():
     descriptions = list(transaction_descriptions(transactions))
-    assert descriptions == [
-        "Перевод организации",
-        "Перевод со счета на счет",
-        "Перевод со счета на счет",
-        "Перевод с карты на карту",
-        "Перевод организации"
-    ]
+    assert descriptions == ["Описание 1", "Описание 2", "Описание 3"]
 
     empty_descriptions = list(transaction_descriptions([]))
     assert empty_descriptions == []
 
 def test_card_number_generator():
-    card_numbers = list(card_number_generator(1, 5))
-    assert card_numbers == [
-        "0000000000000001",
-        "0000000000000002",
-        "0000000000000003",
-        "0000000000000004",
-        "0000000000000005"
-    ]
+    card_numbers = list(card_number_generator(1, 3))
+    assert card_numbers == ["0000000000000001", "0000000000000002", "0000000000000003"]
 
     single_card_number = list(card_number_generator(1, 1))
     assert single_card_number == ["0000000000000001"]
