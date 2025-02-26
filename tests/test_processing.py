a81a6a5604926f38f70487207c93221543f1cb72
wrluu <fikbookmy@gmail.com>
2025-02-26 12:00:39 +0300

    исправила импорт из другой директории

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 import pytest
 from datetime import datetime
 from processing import (filter_by_state, sort_by_date)
